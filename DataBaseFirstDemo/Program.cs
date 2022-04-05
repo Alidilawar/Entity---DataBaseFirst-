ea620c7d3de7c46511353ed6a053637563556921
Alidilawar <59058098+Alidilawar@users.noreply.github.com>
2022-04-06 00:32:04 +0500

    DataBase Model

---
+++ DataBaseFirstDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Context = new DataBaseFirstDemoEntities();
+            var Post = new Post
+            {
+                Body = "Body",
+                DataPublished = DateTime.Now,
+                Title = "title",
+               
+            };
+
+            Context.Posts.Add(Post);
+            Context.SaveChanges();
+        }
+    }
+}
